2dbe373930b2cfac7483505353a7cd4b1ba967e9
Amber Gregory <amber.gregory@numerator.com>
2019-05-16 15:28:40 -0700

    Added init funciton for User objects, faker for fake initial passwords

--- model.py
+++ model.py
@@ -1,10 +1,16 @@
 """Models and database functions for crApp."""
 
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime 
+from faker import Faker
+from faker.providers import misc
 
 # Establish connection to the PostgreSQL database
 db = SQLAlchemy()
 
+# Set up Faker object to generate fake initial passwords for users
+fake = Faker()
+fake.add_provider(misc)
 
 ##################################################################
 # Model definitions
@@ -20,18 +26,31 @@ class User(db.Model):
     email = db.Column(db.String(256), nullable=False)
     gender = db.Column(db.String(30), nullable=True)
     date_of_birth = db.Column(db.Date, nullable=True)
-    created_at = db.Column(db.Date, nullable=False)
-    is_premium = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    is_premium = db.Column(db.Boolean, default=False, nullable=False)
+
+    # Define relationships
+    lists = db.relationship("NamedList")
+    checkins = db.relationship("CheckIn")
+    ratings = db.relationship("Rating", backref=db.backref("user"))
+    
+    def __init__(self, full_name, email, password=None):
+        """Initialize a User object."""
+
+        self.full_name = full_name
+        self.email = email
+        self.created_at = datetime.now()
+        
+        if password:
+            self.password = password
+        else:
+            self.password = fake.password(length=10, special_chars=True, digits=True, 
+                                          upper_case=True, lower_case=True) 
 
     def __repr__(self):
         """Provide helpful User representation when printed."""
 
         return f"<User id={self.user_id} name={self.full_name}>"
-
-    # Define relationships
-    lists = db.relationship("NamedList")
-    checkins = db.relationship("CheckIn")
-    ratings = db.relationship("Rating", backref=db.backref("ratings"))
 
 class Bathroom(db.Model):
     """Bathroom on crApp website."""
@@ -39,19 +58,19 @@ class Bathroom(db.Model):
     __tablename__ = "bathrooms"
 
     bathroom_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
-    latitude = db.Column(db.Float('10,6'), nullable=False)
-    longitude = db.Column(db.Float('10,6'), nullable=False)
+    latitude = db.Column(db.Float(10,6), nullable=False)
+    longitude = db.Column(db.Float(10,6), nullable=False)
     address = db.Column(db.String(200), nullable=True)
-    is_premium = db.Column(db.Boolean, default=False)
-
-    def __repr__(self):
-        """Provide helpful Bathroom representation with printed."""
-
-        return f"<Bathroom id={self.bathroom_id} lat_long={self.latitude},{self.longitude}>"
+    is_premium = db.Column(db.Boolean, default=False, nullable=False)
 
     # Define relationships
     checkins = db.relationship("CheckIn")
     ratings = db.relationship("Rating")
+    
+    def __repr__(self):
+        """Provide helpful Bathroom representation with printed."""
+
+        return f"<Bathroom id={self.bathroom_id} lat_long={self.latitude},{self.longitude}>"
 
 class NamedList(db.Model):
     """Named lists for users to add bathrooms to (fave, least fave, etc)."""
@@ -62,13 +81,13 @@ class NamedList(db.Model):
     list_name = db.Column(db.String(32), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=True)
 
+    # Define relationship
+    list_items = db.relationship("ListItem", backref=db.backref("named_list"))
+    
     def __repr__(self):
         """Provide helpful NamedList representation when printed."""
 
         return f"<List id={self.list_id} name={self.list_name}>"
-
-    # Define relationship
-    list_items = db.relationship("NamedList", backref=db.backref("named_list"))
 
 class ListItem(db.Model):
     """ListItems - bathrooms, user, named list they've been put on."""
@@ -79,7 +98,7 @@ class ListItem(db.Model):
     list_id = db.Column(db.Integer, db.ForeignKey('lists.list_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     bathroom_id = db.Column(db.Integer, db.ForeignKey('bathrooms.bathroom_id'), nullable=False)
-    date_faved = db.Column(db.Date, nullable=False)
+    datetime_faved = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
         """Provide helpful ListItem representation when printed."""
@@ -94,13 +113,13 @@ class CheckIn(db.Model):
     checkin_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     bathroom_id = db.Column(db.Integer, db.ForeignKey('bathrooms.bathroom_id'), nullable=False)
-    checkin_date= db.Column(db.Date, nullable=False)
+    checkin_datetime= db.Column(db.DateTime, nullable=False)
     rating_id = db.Column(db.Integer, db.ForeignKey('ratings.rating_id'), nullable=True)
 
     def __repr__(self):
         """Provide helpful CheckIn representation when printed."""
         
-        return f"<Checkin id={self.checkin_id} user={self.user_id} bathroom={self.bathroom_id} date={self.checkin_date}>"
+        return f"<Checkin id={self.checkin_id} user={self.user_id} bathroom={self.bathroom_id} date={self.checkin_datetime}>"
 
 class Rating(db.Model):
     """User ratings, must have checked in to make a rating."""
@@ -126,11 +145,10 @@ def connect_to_db(app):
     """Connect database to flask app."""
 
     # Configure to use PostgreSQL db
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///ratings'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///crapp'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
-
 
 if __name__ == "__main__":
     # If run interactively, will be in state to work with db directly
